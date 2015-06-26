a7230d3903fa76bc0add704070fdba2cebc3ba3d
bundheim <borge.undheim@navico.com>
2015-06-26 12:48:15 +0200

    Create Test2.py

---
+++ Test2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 13:32:28 2015
+
+@author: borge.undheim
+"""
+
+class TestInterface:  
+    
+    def setup(self):
+    
+    def test(self):
+        
+    def getResult(self):
+        return "f"
+        
+    def getNote(self):
+        return "Test failed because of ..."   
+       
+    def getFiles(self):
+        return None
