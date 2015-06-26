adea131558f6528a479ebb98c6d878a3c5b5db5a
bundheim <borge.undheim@navico.com>
2015-06-26 12:46:30 +0200

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -1,1 +1,21 @@
-print "Test1"
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 13:32:28 2015
+
+@author: borge.undheim
+"""
+
+class TestInterface:  
+    
+    def setup(self):
+    
+    def test(self):
+        
+    def getResult(self):
+        return "p"
+        
+    def getNote(self):
+        return "This is a test execution note made by Test1"   
+       
+    def getFiles(self):
+        return None
